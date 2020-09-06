4ff4eeaed48da636caf0ff1e574993dcbf48a88d
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-09-06 16:38:53 +0100

    Add basic file select button

--- whatsapp_formatter_gui.py
+++ whatsapp_formatter_gui.py
@@ -2,8 +2,27 @@ from formatter_functions import *
 from zipfile import ZipFile
 from glob import glob
 import tkinter as tk
+from tkinter import filedialog
 import os
 
+cwd = os.getcwd()
+zip_file = ""
+
+
+def select_zip():
+    global zip_file
+    zip_file = filedialog.askopenfilename(
+        initialdir=cwd, title="Select an exported chat", filetypes=[("Zip files", "*.zip")])
+
+
 root = tk.Tk()
+root.title("WhatsApp Formatter")
+root.geometry("500x300")
+
+introMessage = tk.Label(root, text="Welcome to the WhatsApp Formatter!", anchor="n")
+introMessage.pack()
+
+zip_select_button = tk.Button(root, text="Select an exported chat", command=select_zip)
+zip_select_button.pack()
 
 root.mainloop()
