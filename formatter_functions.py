c1fab1075126198766841b9ad5d6da272fc5684b
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-09-06 10:37:56 +0100

    Split functions into separate file

--- formatter_functions.py
+++ formatter_functions.py
@@ -1,56 +1,17 @@
+from whatsapp_formatter import recipName, outputDir
 from pydub import AudioSegment
 from datetime import datetime
-from zipfile import ZipFile
-from glob import glob
-import os
 import re
-
-# ===== Initial Setup
-
-cwd = os.getcwd()
-
-print("Welcome to the WhatsApp Formatter!")
-print()
-print(f"Please move the selected zip to {cwd}")
-print()
-input_file = input("Please enter the name of the input zip file: ")
-if not input_file.endswith(".zip"):
-    input_file = f"{input_file}.zip"
-print()
-recipName = input("Please enter the name of the recipient: ")
-print()
-outputDir = input("Please enter a full output directory: ")
-print()
-
-# Extracts selected zip file to /temp/
-zip_ref = ZipFile(input_file)
-print("Unzipping...")
-zip_ref.extractall("temp")
-zip_ref.close()
-print("Unzipped!")
-print()
-
-# Make dir if it doesn't exist
-try:
-    os.mkdir(f"{outputDir}/{recipName}")
-except OSError:
-    pass
-
-# Creates chat_txt_list as list of _chat.txt
-with open("temp/_chat.txt", encoding="utf-8") as f:
-    chat_txt_list = f.read().splitlines()
+import os
 
 audio_extensions = (".opus", ".m4a")  # List of accepted non-mp3 audio files
 image_extensions = (".jpg", ".png", ".webp")  # List of accepted image extensions
 format_dict = {"_": "em", "*": "strong", "~": "del"}  # Dict of format characters and their tags
 
-print("Reformatting...")
+cwd = os.getcwd()
 
 
-# ===== Define functions
-
-
-def html_cleaner(string):  # Get rid of <> in non-attachment messages
+def clean_html(string):  # Get rid of <> in non-attachment messages
     """Remove the characters \"<\" and \">\" in non-attachment messages."""
     string = string.replace("<", "&lt;")
     string = string.replace(">", "&gt;")
@@ -58,7 +19,7 @@ def html_cleaner(string):  # Get rid of <> in non-attachment messages
     return string
 
 
-def tag_replace(string, char, tag):  # Replace char with tags in string
+def replace_tags(string, char, tag):  # Replace char with tags in string
     """Replace selected format character with selected HTML tag."""
     count = 0
     list_message = list(string)
@@ -101,7 +62,7 @@ def reformat(string):
         for x, (char, tag) in enumerate(format_dict.items()):
             if char in string:
                 if string.count(char) % 2 == 0:
-                    string = tag_replace(string, char, tag)
+                    string = replace_tags(string, char, tag)
 
     # ===== Format links
 
@@ -120,7 +81,7 @@ def reformat(string):
 def add_attachments(string):
     """Embed images, videos, and audio."""
     # Parse filename and extension with a RegEx
-    attachment_match = re.search(r"<attached: ([^\.]+)(\.\w+)>$", string)
+    attachment_match = re.search(r"<attached: ([^.]+)(\.\w+)>$", string)
     extension = attachment_match.group(2)
     filename = attachment_match.group(1) + extension
 
@@ -191,61 +152,3 @@ def create_message_block(string):
     string = f"{string}<span class=\"message-info date\">{date}</span>"
 
     return string
-
-
-# ===== Reformat chat_txt into recipName.html
-
-html_file = open(f"{outputDir}/{recipName}.html", "w+", encoding="utf-8")
-
-with open("start_template.txt", encoding="utf-8") as f:
-    start_template = f.readlines()
-
-# Replace recipName in start_template
-for i, line in enumerate(start_template):
-    pos = line.find("{recipName}")
-    if pos != -1:  # If "recipName" is found
-        start_template[i] = line.replace("{recipName}", recipName)  # Replace with var
-
-# Add start template
-for i in start_template:
-    html_file.write(i)
-
-for i in chat_txt_list:
-    # Detect attachments
-    i = i.replace("\u200e", "")  # Clear left-to-right mark
-    distance = i.find(": ")
-    i_list = list(i)
-    if i == "":
-        pass
-    else:
-        if ": <attached: " in i:  # If attachment
-            if i_list[distance + 2] == "<":
-                i = add_attachments(i)
-                html_file.write(i)
-                continue  # next i
-
-    # Write reformatted & complete message to {recipName}.html
-    html_file.write(create_message_block(reformat(html_cleaner(i))))
-
-with open("end_template.txt", encoding="utf-8") as f:
-    end_template = f.readlines()
-
-# Add end template
-for i in end_template:
-    html_file.write(i)
-
-print("Reformatting complete!")
-
-# ===== Clear up /temp/
-
-# Delete remaining files in /temp/ (should just be _chat.txt)
-files = glob("temp/*")
-for f in files:
-    os.remove(f)
-
-os.rmdir("temp")
-
-print()
-print("Process complete!")
-print()
-input("Press enter to exit the program...")
--- whatsapp_formatter_gui.py
+++ whatsapp_formatter_gui.py
@@ -1,10 +1,8 @@
-from pydub import AudioSegment
-from datetime import datetime
+from formatter_functions import *
 from zipfile import ZipFile
 from glob import glob
 import tkinter as tk
 import os
-import re
 
 root = tk.Tk()
 
