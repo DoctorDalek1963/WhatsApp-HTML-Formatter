27fc04db2ceb8b954e793db5d29842d36c2cdc3b
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-09-10 17:18:29 +0100

    Add multi zip compatibility to command line version

--- whatsapp_formatter.py
+++ whatsapp_formatter.py
@@ -2,31 +2,34 @@ from formatter_functions import write_to_file, extract_zip
 import os
 
 cwd = os.getcwd()
+quit_flag = False
 
 print("Welcome to the WhatsApp Formatter!")
-print()
-print(f"Please move the selected zip to {cwd}")
-print()
-input_file = input("Please enter the name of the input zip file: ")
-if not input_file.endswith(".zip"):
-    input_file = f"{input_file}.zip"
-print()
-outputDir = input("Please enter a full output directory: ")
-print()
-recipName = input("Please enter the name of the recipient (case sensitive): ")
-print()
 
-print("Unzipping...")
-extract_zip(input_file)
-print("Unzipped!")
-print()
+while not quit_flag:
+    print()
+    print(f"Please move the selected zip to {cwd}")
+    print()
+    input_file = input("Please enter the name of the input zip file: ")
+    if not input_file.endswith(".zip"):
+        input_file = f"{input_file}.zip"
+    print()
+    outputDir = input("Please enter a full output directory: ")
+    print()
+    recipName = input("Please enter the name of the recipient (case sensitive): ")
+    print()
 
-print("Reformatting...")
+    print("Unzipping...")
+    extract_zip(input_file)
+    print("Unzipped!")
+    print()
 
-write_to_file(recipName, outputDir)  # All the heavy lifting is done by this function
+    print("Reformatting...")
 
-print("Reformatting complete!")
-print()
-print("Process complete!")
-print()
-input("Press enter to exit the program...")
+    write_to_file(recipName, outputDir)  # All the heavy lifting is done by this function
+
+    print("Reformatting complete!")
+    print()
+    print("Process complete!")
+    print()
+    quit_flag = input("Type \'1\' to quit, press enter to continue with another zip... ")
