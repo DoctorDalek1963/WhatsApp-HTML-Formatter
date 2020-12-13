c52b2cefbefedad5cb1f8d042eef68e0ab39c3c7
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2020-12-13 22:44:44 +0000

    Update enlarge_images.js

--- Library/enlarge_images.js
+++ Library/enlarge_images.js
@@ -1,1 +1,4 @@
 // TODO: Write JS to enlarge an image and center it when the image is clicked
+// Intent: On click, the image will enlarge to fit 80% (subject to change) of the viewport's height
+// It will be centered in the viewport and the background will be darkened
+// The original image will stay in the same place at the same size
