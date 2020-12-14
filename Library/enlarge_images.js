7550fb05d0338f78f5d88dee0dfd625ec91fadad
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2020-12-14 20:57:58 +0000

    Fix enlarge image on click function

--- Library/enlarge_images.js
+++ Library/enlarge_images.js
@@ -1,12 +1,7 @@
-// TODO: Write JS to enlarge an image and center it when the image is clicked
-// Intent: On click, the image will enlarge to fit 80% (subject to change) of the viewport's height
-// It will be centered in the viewport and the background will be darkened
-// The original image will stay in the same place at the same size
-
-$('img.small').click(function() {
-    $(this).after('<img class="large" src="' + $(this).attr('src') + '" alt="ENLARGED IMAGE">');
-});
-
-$('img.large').click(function() {
-    $(this).remove();
+$('img').click(function() {
+    if ($(this).hasClass('small')) {
+        $(this).removeClass('small').addClass('large');
+    } else {
+        $(this).removeClass('large').addClass('small');
+    }
 });
