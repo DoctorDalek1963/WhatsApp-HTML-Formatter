3d9710fe745736057eb52a7855174e1f09e73ab3
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2020-12-20 12:22:09 +0000

    Update enlarge_images.js

--- Library/enlarge_images.js
+++ Library/enlarge_images.js
@@ -2,7 +2,7 @@ $(function () {
     $('img').on('click', function() {
         if ($(this).hasClass('small')) {
             $(this).removeClass('small').addClass('large');
-            $(this).animate({'max-height': '40vw', 'max-width': '80vw'}, 200);
+            $(this).animate({'max-height': '80vh', 'max-width': '80vw'}, 200);
         } else {
             $(this).removeClass('large').addClass('small');
             $(this).animate({'max-height': '400px', 'max-width': '800px', 'width': 'auto'}, 200);
