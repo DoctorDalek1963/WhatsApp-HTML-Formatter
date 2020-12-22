fd35c2e573bccc7af10d89189010c2ac4ff18809
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2020-12-22 19:00:50 +0000

    Make group chat checkbox work properly

--- multiple_gui.py
+++ multiple_gui.py
@@ -17,16 +17,15 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from formatter_functions import process_list
-from tkinter import filedialog, StringVar
+from tkinter import filedialog, StringVar, IntVar
 import tkinter as tk
 import _tkinter
 import threading
 import os
 
 cwd = os.getcwd()
-inputZip = outputDir = recipientName = ''
-startProcessingFlag = processingFlag = endProcessingFlag = False
-addToListFlag = False
+inputFile = outputDir = recipientName = ''
+startProcessingFlag = processingFlag = endProcessingFlag = addToListFlag = False
 groupChat = False
 allChatsList = []
 
@@ -49,9 +48,17 @@ gap_y_padding = (5, 15)
 
 
 def select_zip():
-    global inputZip
-    inputZip = filedialog.askopenfilename(initialdir=cwd, title='Select an exported chat',
-                                          filetypes=[('Zip files', '*.zip')])
+    global inputFile
+    inputFile = filedialog.askopenfilename(initialdir=cwd, title='Select an exported chat',
+                                           filetypes=[('Zip files', '*.zip')])
+
+
+def assign_group_chat_bool():
+    global groupChat
+    if group_chat_checkbox_var.get() == 1:
+        groupChat = True
+    else:
+        groupChat = False
 
 
 def select_output_dir():
@@ -88,6 +95,8 @@ selected_zip_var = StringVar()
 selected_output_var = StringVar()
 processing_string_var = StringVar()
 
+group_chat_checkbox_var = IntVar()
+
 
 # ===== Create widgets
 
@@ -98,8 +107,8 @@ description_label = tk.Label(root, text=descriptionText)
 select_zip_button = tk.Button(root, text='Select an exported chat', command=select_zip)
 selected_zip_label = tk.Label(root, textvariable=selected_zip_var)
 
-group_chat_checkbox = tk.Checkbutton(root, text='Group chat', variable=groupChat,
-                                     onvalue=True, offvalue=False)
+group_chat_checkbox = tk.Checkbutton(root, text='Group chat', variable=group_chat_checkbox_var,
+                                     onvalue=1, offvalue=0, command=assign_group_chat_bool)
 
 sender_name_box_label = tk.Label(root, text='Enter the name of the recipient:')
 enter_sender_name = tk.Entry(root)
@@ -200,16 +209,16 @@ def update_loop():
                 recipientName = ''
 
                 select_zip_button.config(state='disabled')
-                select_output_button.config(state='disabled')
                 enter_sender_name.config(state='disabled')
+                select_output_button.config(state='disabled')
                 exit_button.config(state='disabled')
 
             if endProcessingFlag:
                 processing_string_var.set('')
 
                 select_zip_button.config(state='normal')
-                select_output_button.config(state='normal')
                 enter_sender_name.config(state='normal')
+                select_output_button.config(state='normal')
                 exit_button.config(state='normal')
 
                 endProcessingFlag = False
