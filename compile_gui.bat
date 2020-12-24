97eda0e99aa135e0417164a20440f68104eb0ab0
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2020-12-24 20:25:57 +0000

    Put whole pipeline in one file

--- compile_gui.bat
+++ compile_gui.bat
@@ -1,3 +1,1 @@
-python compile_gui_1.py
-pyinstaller formatter_gui.py -wF -n WhatsApp_Formatter --distpath ./compile_temp -i Library/favicon.ico
-python compile_gui_2.py
+python compile_gui.py
---
+++ compile_gui.py
@@ -0,0 +1,32 @@
+"""Fully compile the GUI version of the formatter with pyinstaller."""
+
+import shutil
+import os
+
+# Create temporary directory to hold everything
+os.mkdir('compile_temp')
+
+# Copy dependencies to temporary directory
+shutil.copy('start_template.txt', 'compile_temp/')
+shutil.copy('end_template.txt', 'compile_temp/')
+shutil.copytree('Library', 'compile_temp/Library')
+
+# Remove jsconfig file, which only exists on my machine
+try:
+    os.remove('compile_temp/Library/jsconfig.json')
+except FileNotFoundError:
+    pass
+
+os.system('cmd /c "pyinstaller formatter_gui.py -wF -n WhatsApp_Formatter --distpath ./compile_temp -i '
+          'Library/favicon.ico"')
+
+# Remove spec file (I don't think there's a flag to tell pyinstaller to not generate this)
+os.remove('WhatsApp_Formatter.spec')
+
+# Zip up compiled program with dependencies
+shutil.make_archive('WhatsApp_Formatter', 'zip', 'compile_temp')
+
+# Clear and remove unnecessary directories
+shutil.rmtree('build')
+shutil.rmtree('compile_temp')
+shutil.rmtree('__pycache__')
