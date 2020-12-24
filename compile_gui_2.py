2abdaac6b3172d6c4a9a08d88fd2c871f5fc3042
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2020-12-24 14:32:26 +0000

    Reformat compile_gui_2.py

--- compile_gui_2.py
+++ compile_gui_2.py
@@ -5,16 +5,13 @@ Zips up folder for release and cleans up."""
 import shutil
 import os
 
+# Remove spec file (I don't think there's a flag to tell pyinstaller to not generate this)
 os.remove('WhatsApp_Formatter.spec')
-
-# Clear and remove the build directory
-shutil.rmtree('build')
 
 # Zip up compiled program with dependencies
 shutil.make_archive('WhatsApp_Formatter', 'zip', 'compile_temp')
 
-# Clear and remove temporary directory
+# Clear and remove unnecessary directories
+shutil.rmtree('build')
 shutil.rmtree('compile_temp')
-
-# Clear and remove __pycache__ byte code
 shutil.rmtree('__pycache__')
